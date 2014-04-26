9e7441ae3bf1434b466b61f6515722ff69bfb63d
Justin Kutch <Justin.Kutch@Disney.com>
2014-04-26 15:55:39 -0700

    added new game form

--- app/games.js
+++ app/games.js
@@ -1,7 +1,20 @@
-angular.module('app').controller('games', ['$scope', '$location' ,function($scope, $location){
+angular.module('app').controller('games', ['$scope', '$location', '$firebase' ,function($scope, $location, $firebase){
 	if(!$scope.auth.user) {
 		$location.path('/');
 		$scope.alert.class = "danger";
 		$scope.alert.message = "Please sign in to view your games";
 	}
+
+	var ref = new Firebase('https://initative.firebaseio.com/');
+	$scope.firebase = $firebase(ref);
+
+	$scope.addGame = function() {
+		if(!this.showAddForm) {
+			this.showAddForm = true;
+		}else {
+			this.showAddForm = false;
+			alert('add game');
+		}
+	}
+
 }]);
