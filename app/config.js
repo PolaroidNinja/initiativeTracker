05b29043dc12827bec1394b543afc94fbb575090
Justin Kutch <Justin.Kutch@Disney.com>
2014-04-29 23:50:31 -0700

    Basic Game View started

--- app/config.js
+++ app/config.js
@@ -17,3 +17,27 @@ angular.module('app').config(function($routeProvider){
 			redirectTo:'/'
 		});
 });
+
+app.filter('orderObjectBy', function(){
+ return function(input, attribute) {
+ 	var reverse = false;
+    if (!angular.isObject(input)) return input;
+
+    if(attribute.substring(0,1) === "-"){
+		reverse = true;
+		attribute = attribute.substr(1);
+    }
+
+    var array = [];
+    for(var objectKey in input) {
+        array.push(input[objectKey]);
+    }
+
+    array.sort(function(a, b){
+        a = parseInt(a[attribute]);
+        b = parseInt(b[attribute]);
+        return a - b;
+    });
+    return (reverse === true) ? array.reverse() : array;
+ }
+});
--- app/play.js
+++ app/play.js
@@ -1,3 +1,30 @@
 angular.module('app').controller('play', ['$scope', '$location', '$routeParams', '$firebase' ,function($scope, $location, $routeParams, $firebase){
 	$scope.gameID = $routeParams.gameID;
+
+	var ref = new Firebase('https://initative.firebaseio.com/games/' + $scope.gameID);
+	var upSort = {"icon": 'up', "sort": "-initiative"};
+	var dwnSort = {"icon": 'down', "sort": "initiative"};
+	$scope.firebase = $firebase(ref);
+	$scope.orderType = upSort;
+
+	$scope.addParticipant = function() {
+		var newParticipant = {
+			"name": $scope.participantName,
+			"initiative": $scope.participantInit
+		};
+		if(!this.showAddForm) {
+			this.showAddForm = true;
+		}else {
+			ref.child('participants').push(newParticipant);
+			this.showAddForm = false;
+		}
+	}
+
+	$scope.changeSort = function(){
+		if(this.orderType == upSort){
+			this.orderType = dwnSort;
+		}else {
+			this.orderType = upSort;
+		}
+	}
 }]);
