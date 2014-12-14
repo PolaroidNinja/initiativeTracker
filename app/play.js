69426025cb6c7348a87dafb4d17ee908cd15774b
Justin Kutch <Justin.Kutch@Disney.com>
2014-12-14 06:14:55 -0800

    Created service and added basic sorting to list

--- app/play.js
+++ app/play.js
@@ -1,16 +1,17 @@
-angular.module('app').controller('play', ['$scope', '$location', '$routeParams', '$firebase', '$filter' , 'game'
-	,function($scope, $location, $routeParams, $firebase, $filter, $game){
+angular.module('app').controller('play', ['$scope', '$location', '$routeParams', '$firebase', '$filter' , 'gameService'
+	,function($scope, $location, $routeParams, $firebase, $filter, gameService){
 	var gameID = $routeParams.gameID;
 	var orderBy = $filter('orderBy');
-	var participants = $game.setId(gameID).participants.get().$asObject();
-	var displayArray = $game.participants.get().$asArray();
+	var participants = gameService.setId(gameID).participants.get().$asObject();
+	var displayArray = gameService.participants.get().$asArray();
 
 	displayArray.$loaded().then(function(){
 		order( 'initiative' , true);
-		$game.participants.getByName('Leo').then(function(e){
+		gameService.participants.getByName('Leo').then(function(e){
 			console.log(e);
 		});
 	});
+
 
 	$scope.addParticipant = function() {
 		var newParticipant = {
@@ -28,6 +29,14 @@ angular.module('app').controller('play', ['$scope', '$location', '$routeParams',
 	var order = function(predicate, reverse){
 		var pa = orderBy(displayArray, predicate, reverse);
 		$scope.participantsArray = pa;
+	}
+
+	$scope.sorted = function($item, $partFrom, $partTo, $indexFrom, $indexTo) {
+		console.log($item);
+		console.log($partFrom);
+		console.log($partTo);
+		console.log($indexFrom);
+		console.log($indexTo);
 	}
 
 	$scope.order = order;
